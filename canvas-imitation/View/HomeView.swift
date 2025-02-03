efbd9451101d8780ba7048cf0aa05eec66b88cdc
Luis Amorim <luis@amorim.bio.br>
2025-02-03 13:58:22 -0300

    Created Model, View, and ViewModel for the Canvas, along with general Xcode project setup. - Developed Canvas and CanvasItem models to represent data for each canvas and its items. - Created CanvasViewModel to handle the logic of adding, moving, resizing items, and managing snap functionality. - Set up basic views to interact with the Canvas and display items. - Configured general Xcode project settings, including initial setup for the Core Data model and Info.plist adjustments for internet access.

---
+++ canvas-imitation/View/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  canvas-imitation
+//
+//  Created by Luis Amorim on 03/02/25.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
