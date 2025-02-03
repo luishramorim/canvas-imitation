71cc7d1f7ff4faf8f572b3e1063e96dfe7744933
Luis Amorim <luis@amorim.bio.br>
2025-02-03 13:28:13 -0300

    Initial Commit

---
+++ canvas-imitation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  canvas-imitation
+//
+//  Created by Luis Amorim on 03/02/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ canvas-imitation/canvas_imitationApp.swift
@@ -0,0 +1,17 @@
+//
+//  canvas_imitationApp.swift
+//  canvas-imitation
+//
+//  Created by Luis Amorim on 03/02/25.
+//
+
+import SwiftUI
+
+@main
+struct canvas_imitationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
